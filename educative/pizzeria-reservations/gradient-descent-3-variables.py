635da7e0a516187d2893f464a8662955b9b7cf02
Alejo Silva <alejo.lautaro@hotmail.com>
2025-11-02 18:58:45 -0300

    Upgraded the gradient function and all the functions that depends on it to support multiple variables

--- educative/pizzeria-reservations/gradient-descent-3-variables.py
+++ educative/pizzeria-reservations/gradient-descent-3-variables.py
@@ -20,3 +20,27 @@ print(X[:2])
 
 Y = y.reshape(-1, 1)
 print(Y.shape)
+
+w = np.zeros((X.shape[1], 1))
+w.shape  # => (3, 1)`
+
+
+# w.shape[0] == number of rows, w.shape[1] == number of columns
+def predict(X, w):
+    return np.matmul(X, w)
+
+
+def loss(X, Y, w):
+    return np.average((predict(X, w) - Y) ** 2)
+
+
+a_number = loss(X, Y, w)
+a_number.shape  # => ()
+
+
+def gradient(X, Y, w):
+    return 2 * np.matmul(X.T, (predict(X, w) - Y)) / X.shape[0]
+
+
+# X.T means that the X was transposed
+# we are not using the average function here, but we are performing the average manually
